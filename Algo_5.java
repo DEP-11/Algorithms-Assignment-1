443e9013441da72105470f652cab7a3dd9f93387
Nilan Dinushka <nilandinushka5@gmail.com>
2023-08-13 10:42:49 +0530

    completed algo-5

---
+++ Algo_5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Algo_5 {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        loop:
+        do{
+            System.out.print("Enter the text: ");
+            String text = scanner.nextLine().strip();
+            if(text.isBlank()){
+                System.out.println("Invali Text! Enter again to invert.");
+                continue loop;
+            }
+            char[] textToChar = text.toCharArray();
+            String invertText = "";
+            for (int i = 0; i < textToChar.length; i++) {
+                invertText = invertText + textToChar[textToChar.length-1-i];
+                
+            }
+            System.out.println(invertText);
+
+
+        }while(true);
+    }
+}

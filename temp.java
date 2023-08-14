7f5397162453b9458ecb4fb836d6c4844e66fd80
Nilan Dinushka <nilandinushka5@gmail.com>
2023-08-14 08:20:19 +0530

    completed algo-10

--- temp.java
+++
@@ -1,9 +0,0 @@
-public class temp {
-    public static void main(String[] args) {
-        String name = "Nilan Dinushka";
-        name= split(name);
-        for (int i = 0; i < x.length; i++) {
-            System.out.println((int) x[i]);
-        }
-    }
-}
